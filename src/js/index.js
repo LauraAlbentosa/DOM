63598fb0f1773bbd70424d2d922ce24fbed390bd
lauraosallor <lauraosallor@gmail.com>
2024-06-21 09:27:17 +0200

    correccion

--- src/js/index.js
+++ src/js/index.js
@@ -35,14 +35,13 @@ para imprimir una etiqueta de ehmtl como objeto se usa console.dir */
 
 const h3Element = document.getElementById('start-point');
 
-/*console.log(
-  `Soy un h3 con el id ${h3Element.id} y la clase ${h3Element.classList}, mi padre es un header con el id ${h3Element.parentElement.id} y la clase ${h3Element.parentElement.classList}, mi hermano es un h1 con el id ${h3Element.parentElement.children[0].id} y la clase ${h3Element.parentElement.children[0].classList[0]} y el contenido ${h3Element.parentElement.children[0].textContent}, mi otro hermano es un h2 con el id ${h3Element.previousElementSibling.id} y la clase ${h3Element.previousElementSibling.classList[0]} y el contenido ${h3Element.previousElementSibling.textContent}`
+console.log(
+  `Soy un h3 con el id ${h3Element.id} y la clase ${h3Element.classList}, mi padre es un header con el id ${h3Element.parentElement.id} y la clase ${h3Element.parentElement.classList}, mi hermano es un h1 con el id ${h3Element.parentElement.children[0].id} y la clase ${h3Element.parentElement.children[0].classList[0]} y ${h3Element.parentElement.children[0].classList[1]}y el contenido ${h3Element.parentElement.children[0].textContent}, mi otro hermano es un h2 con el id ${h3Element.previousElementSibling.id} y la clase ${h3Element.previousElementSibling.classList[0]} y ${h3Element.previousElementSibling.classList[1]} y el contenido ${h3Element.previousElementSibling.textContent}`
 );
-*/
 
 // - Partiendo de esta lista, intercambia los textos de los li, deberás tener este resultado sin modificar a mano el HTML. Importante, no hay que mover el <li> sólo cambiar el contenido.
 
-/*const firstUlElement = document.getElementById('first-list');
+const firstUlElement = document.getElementById('first-list');
 const secondUlElement = document.getElementById('second-list');
 
 const first = firstUlElement.textContent;
@@ -50,7 +49,6 @@ const second = secondUlElement.textContent;
 
 firstUlElement.textContent = second;
 secondUlElement.textContent = first;
-*/
 
 /*
 - Usando este HTML muestra por consola el número de etiquetas que hay de cada cosa.
